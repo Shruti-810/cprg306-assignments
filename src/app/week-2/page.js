05e68ecbb3e71d333e9712b6e3282e26e7bc0295
Shruti-810 <shrutimak810@gmail.com>
2024-09-18 11:03:44 -0600

    Week 2 Assignment

---
+++ src/app/week-2/page.js
@@ -0,0 +1,12 @@
+import StudentInfo from "./student-info";
+
+let Page = () => {
+    return(
+        <>
+        <h1>Shopping List</h1>
+        <StudentInfo />
+        </>
+    );
+}
+
+export default Page
---
+++ src/app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+let StudentInfo = () => {
+    return(
+        <>
+        <h1>Shruti Makwana</h1>
+        <Link href="https://github.com/Shruti-810/cprg306-assignments.git" target="_blank">Github Repository</Link>
+        </>
+    );
+}
+
+export default StudentInfo
